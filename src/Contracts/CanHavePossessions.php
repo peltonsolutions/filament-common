928afc5862351e4d4abac574367ef4f3c81fe3a0
Nathan Pelton <npelton@gmail.com>
2024-03-14 16:35:48 -0400

    added getCurrent to CanHavePossessions

--- src/Contracts/CanHavePossessions.php
+++ src/Contracts/CanHavePossessions.php
@@ -7,6 +7,8 @@ use PeltonSolutions\FilamentCommon\Interfaces\HasName;
 interface CanHavePossessions extends HasName
 {
 
+	static public function getCurrent(): ?self;
+
 	public function addPossession(CanBePossessed $possession);
 
 	public function getKey();
